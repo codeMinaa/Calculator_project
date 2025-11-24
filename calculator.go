ed36eb576ebf73e97e2fb1099cfed22cdf6e12c2
samuameh <sammypraise4christ@gmail.com>
2025-11-24 17:02:04 +0100

    secondnumber

--- calculator.go
+++ calculator.go
@@ -1,7 +1,16 @@
 package main
 
-func main() {
-	// testing
-	// chokoh's branch
-	// Added a new branch
+import "fmt"
+
+func getSecondNumber() int {
+	var b int
+	fmt.Print("Enter Second Number:.")
+	_, err = fmt.Scanln(&b)
+	if err == nil {
+		fmt.Print(b)
+	} else {
+		fmt.Println("Invalid Input!")
+
+	}
+	return b
 }
